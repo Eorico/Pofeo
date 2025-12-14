4475bdffd30a8f6d74e4c18eb94aa7c419ba2886
Eorico Gonzales <eoricogonzales@gmail.com>
2025-12-14 18:37:42 +0800

    mobile view restriction is fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Nav from "./components/NavBar";
 import Home from "./components/navlinks/Home";
 import About from "./components/navlinks/About";
@@ -8,6 +9,53 @@ import Footer from "./components/navlinks/Footer";
 import './App.css';
 
 export default function App() {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const checkingScreen = () => {
+      setIsMobileView(window.innerWidth <= 768);
+    }
+
+    checkingScreen();
+    window.addEventListener("resize",checkingScreen);
+    return () => window.removeEventListener("resize", checkingScreen);
+  }, []);
+
+  if (isMobileView) {
+   return ( <div
+      style={{
+        height: "100vh",
+        width: '100%',
+        background: '#0f172a',
+        color: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        padding: '20px'
+      }}
+    >
+      <h2
+        style={{
+          fontSize: '2rem',
+          marginBottom: '10px'
+        }}
+      >
+        NOTE: <br /> MOBILE VIEW IS NOT AVAILABLE AT THE MOMENT..
+      </h2>
+      <p
+        style={{
+          fontSize: '1.1rem',
+          opacity: 0.85,
+        }}
+      >
+        Please view this website on a PC or Laptop for the best experience.
+      </p>
+    </div>
+   )
+  }
+
   return (
     <Router>
       <div className="app">
